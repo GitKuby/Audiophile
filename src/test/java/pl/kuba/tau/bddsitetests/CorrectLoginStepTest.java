b7134399bb4a947283e3a039b099a194e9f6f991
Jakub Rączka <jakub.raczka[at]gmail.com>
2017-12-12 21:41:15 +0100

    fix: logout after test

--- src/test/java/pl/kuba/tau/bddsitetests/CorrectLoginStepTest.java
+++ src/test/java/pl/kuba/tau/bddsitetests/CorrectLoginStepTest.java
@@ -6,16 +6,19 @@ import cucumber.api.java.en.When;
 import static junit.framework.Assert.assertTrue;
 import org.openqa.selenium.WebDriver;
 import pl.kuba.tau.pages.LoginPage;
+import pl.kuba.tau.pages.RegisterPage;
 
 public class CorrectLoginStepTest {
 
     private static final LoginPage LOGIN_PAGE;
+    private static final RegisterPage REGISTER_PAGE;
     private static final WebDriver DRIVER = DriverFactory.getDriver();
     private final static String EMAIL = "takiegomaila@niktniepoda.pl";
     private final static String PASS = "secret";
 
     static {
         LOGIN_PAGE = new LoginPage(DRIVER);
+        REGISTER_PAGE = new RegisterPage(DRIVER);
     }
 
     @Given("^login page form$")
@@ -33,6 +36,7 @@ public class CorrectLoginStepTest {
     @Then("^we should see account page$")
     public void we_should_see_account_page() {
         assertTrue(LOGIN_PAGE.isLoginSuccessful());
+        REGISTER_PAGE.getLogoutButton().click();
     }
 
 }
